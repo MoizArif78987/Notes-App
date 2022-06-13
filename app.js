667c10c3bfef39cdec960832d3123643524faaa1
MoizArif78987 <106541549+MoizArif78987@users.noreply.github.com>
2022-06-13 12:45:39 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -5,24 +5,50 @@ showNotes();
 
 let addbtn=document.getElementById('addbtn').addEventListener("click",function(e){
     let addTxt= document.getElementById('addTxt');
-    if (addTxt.textLength == 0) {
-        alert("Please write something in text box first!")
+    let addTitle =document.getElementById('addTitle');
+    if (addTxt.textLength == 0 || addTitle.textLength == 0) {
+        if(addTxt.textLength==0 && addTitle.textLength!=0)
+        {
+            alert("Please write something in Create Note box !");
+          addTxt.setAttribute('class','form-control is-invalid');
+        }
+    else if(addTxt.textLength!=0 && addTitle.textLength==0)
+    {
+        alert("Please write something in Add Note Title box !");
+        addTitle.setAttribute('class','form-control is-invalid');
     }
     else
     {
+        alert("Please enter both note and tile");
+        addTxt.setAttribute('class','form-control is-invalid');
+        addTitle.setAttribute('class','form-control is-invalid');
+    }
+    }
+    else
+    {
+        addTxt.setAttribute("class","form-control");
+        addTitle.setAttribute("class","form-control");
         let notes=localStorage.getItem("notes");
+        let titles=localStorage.getItem("titles");
         if(notes == null) {
             notesObj=[];
+            titleObj=[];
         }
         else {
             notesObj=JSON.parse(notes);
+            titleObj=JSON.parse(titles);
         }
+        //saving note
         console.log(addTxt.value);
         notesObj.push(addTxt.value);
         localStorage.setItem("notes", JSON.stringify(notesObj));
         addTxt.value="";
         console.log(notesObj);
-
+        //saving title
+        titleObj.push(addTitle.value);
+        localStorage.setItem("titles", JSON.stringify(titleObj));
+        addTitle.value="";
+        console.log(titleObj);
         showNotes();
     }
     });
@@ -30,12 +56,16 @@ let addbtn=document.getElementById('addbtn').addEventListener("click",function(e
     // function  to show notes i.e., elements of the local storage
     function showNotes()
     {
+        let titleObj;
         let notes=localStorage.getItem("notes");
+        let titles=localStorage.getItem("titles");
         if(notes == null) {
             notesObj=[];
+            titleObj==[];
         }
         else {
             notesObj=JSON.parse(notes);
+            titleObj=JSON.parse(titles);
         }
             let html="";
             notesObj.forEach(function(element, index) {
@@ -43,7 +73,7 @@ let addbtn=document.getElementById('addbtn').addEventListener("click",function(e
                 // editing the div by my-3 and mx-3 to create 3 spaces horizontally and vertically in the notes
 
                 html += `<div class="card border-dark mb-3 my-3 mx-3 notecard" style="max-width: 18rem;">
-                <div class="card-header">Note ${index+1}</div>
+                <div class="card-header"><b>${titleObj[index]}</b></div>
                 <div class="card-body text-dark">
                   <h5 class="card-title"></h5>
                   <p class="card-text">${element}</p>
@@ -67,9 +97,16 @@ let addbtn=document.getElementById('addbtn').addEventListener("click",function(e
 
 //Deleting Notes
 function deleteNote(index) {
+    let titleObj;
     let notes=localStorage.getItem('notes');
     let notesObj=JSON.parse(notes);
     notesObj.splice(index,1);
+
+    let titles=localStorage.getItem('titles');
+    titleObj=JSON.parse(titles);
+    console.log(titleObj);
+    titleObj.splice(index,1);
+    localStorage.setItem('titles',JSON.stringify(titleObj));
     localStorage.setItem('notes',JSON.stringify(notesObj));
     showNotes();
 }
